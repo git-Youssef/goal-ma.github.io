fcaf0836773c286c9aa43fdee71fe2cfadea2e1e
Youssef Karfouh <63546757+git-Youssef@users.noreply.github.com>
2021-10-11 15:33:19 +0100

    upload assets folder

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+    $('.slider_wrapper').slick({
+        dots: true,
+        prevArrow: '<button class="prev-arrow"><i class="fas fa-chevron-left"></i></button>',
+        nextArrow: '<button class="next-arrow"><i class="fas fa-chevron-right"></i></button>',
+        autoplay : true,
+    });
+
+})
+
+
+
+// icon toggler 
+
+let btn_toggler = document.querySelector(".btn_toggler");
+
+btn_toggler.addEventListener("click", function () {
+    this.classList.toggle("opened");
+    document.querySelector(".navigation").classList.toggle("translate");
+})
+
+
+// on scroll add fixed-top class to header 
+window.addEventListener('scroll', function (e) {
+
+    if (this.scrollY > 100) {
+        document.querySelector("header").classList.add("fixed-top");
+    } else {
+        document.querySelector("header").classList.remove("fixed-top");
+    }
+
+})
